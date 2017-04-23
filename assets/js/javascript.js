6eef62a32089a3aeca231cafab86061882bca909
Joe Mulick <joemulick@gmail.com>
2017-04-22 21:26:15 -0700

    functionality 100%. needs additional styling

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,18 +1,5 @@
 /* Javasccript & jquery */
 
-/*
---When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page. 
-
---When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
---Under every gif, display its rating (PG, G, so on). 
-
-   * This data is provided by the GIPHY API.
-   * Only once you get images displaying with button presses should you move on to the next step.
-
---Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-*/
-
 $(document).ready(function(){
 
 var topButtons = ["Zoolander","Silicon Valley","It's Happening","Tropic Thunder","And its gone","Dumb and Dumber","Anchorman","Ace Ventura","Office Space","Boss Baby","Waynes World","Pee Wees Big Adventure","Monty Python and the Holy Grail","Blazing Saddles"];
@@ -22,8 +9,8 @@ renderButtons();
 
 // When you click the gifs that were rendered to the page you change the data state from animate to still
 
-$(".gif").on("click", function() {
 
+function statusChange(){
       // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
       var state = $(this).attr("data-state");
 
@@ -34,12 +21,13 @@ $(".gif").on("click", function() {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
       }
-    });
+}
 
 function renderGifs() {
 
-        var gifName = $(this).attr("data-name");
+        $('#div-main-section').empty();
 
+        var gifName = $(this).attr("data-name");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&limit=10&api_key=dc6zaTOxFJmzC";
 
         // Creating an AJAX call for the specific gif button being clicked
@@ -59,7 +47,7 @@ function renderGifs() {
           var gifGifURL = response.data[i].images.fixed_height.url;
           var gifState = "still";
 
-          var gifImage = $('<img>').attr("src", gifImageURL).attr("data-animate", gifGifURL).attr("data-still", gifImageURL).attr("data-state", gifState).css('display','block');
+          var gifImage = $('<img>').attr("src", gifImageURL).attr("data-animate", gifGifURL).attr("data-still", gifImageURL).attr("data-state", gifState).css('display','block').addClass('gif');
           var ratingContent = $("<p>Rating: " + rating + "</p>");
 
           gifDiv.append(gifImage);
@@ -99,13 +87,13 @@ function renderGifs() {
         topButtons.push(gifVar);
         // Calling renderButtons which handles the processing of our gif array
         renderButtons();
-
       });
-
       // Adding a click event listener to all elements with a class of "gif"
       $(document).on("click", ".gifButton", renderGifs);
       // Calling the renderButtons function to display the intial buttons
       renderButtons();
+
+      $(document).on("click", ".gif", statusChange);
 
 });
 
